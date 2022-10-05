733ee0a548958e4f56aa066f6bbe0581ba0b4d76
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2022-10-05 22:22:44 +0200

    wip

--- app.lua
+++ app.lua
@@ -11,7 +11,8 @@ function dump(o)
     end
 end
 
-function iterate_coalition(coalition, debug)
+function iterate_coalition_get_radios(coalition, debug)
+    local radios = {}
     for i1, side in pairs(coalition) do
         if debug then
             print("i1 -> " .. i1)
@@ -39,16 +40,31 @@ function iterate_coalition(coalition, debug)
                                         print("               i6 unit_index-> " .. i6)
                                     end
                                     if (singleton["skill"] == "Client") then
-                                    --    CLIENT SINGELTON!!!!
-                                        print("side -> " .. i1)
-                                        print("   country index -> " .. i3)
-                                        print("   type -> " .. i4)
-                                        print("   group index -> " .. i5)
-                                        print("   unit index-> " .. i6)
-                                        print("   unit type-> " .. singleton["type"])
-                                        print("   unit name-> " .. singleton["name"])
-                                        print("   unit callsign-> " .. singleton["callsign"]["name"])
-                                        print("   unit modex-> " .. singleton["onboard_num"])
+                                        local radio = {}
+                                        radio["order"] = {
+                                            ["i1"] = i1,
+                                            ["i2"] = i2,
+                                            ["i3"] = i3,
+                                            ["i4"] = i4,
+                                            ["i5"] = i5,
+                                            ["i6"] = i6,
+                                        }
+                                        radio["type"] = singleton["type"]
+                                        radio["id"] = singleton["type"] .. "/" .. singleton["name"] .. "/" ..  singleton["callsign"]["name"] .. "/" ..  singleton["onboard_num"]
+                                        radio["data"] = singleton["Radio"]
+                                        table.insert(radios, radio)
+                                        --    CLIENT SINGELTON!!!!
+                                        if debug then
+                                            print("side -> " .. i1)
+                                            print("   country index -> " .. i3)
+                                            print("   type -> " .. i4)
+                                            print("   group index -> " .. i5)
+                                            print("   unit index-> " .. i6)
+                                            print("   unit type-> " .. singleton["type"])
+                                            print("   unit name-> " .. singleton["name"])
+                                            print("   unit callsign-> " .. singleton["callsign"]["name"])
+                                            print("   unit modex-> " .. singleton["onboard_num"])
+                                        end
                                     end
                                 end
                             end
@@ -59,7 +75,10 @@ function iterate_coalition(coalition, debug)
         end
 
     end
+    return radios
 end
+
+
 print("Start script")
 
 loaded_chunk = assert(loadfile("mission.lua"))
@@ -68,6 +87,6 @@ coalition = mission["coalition"]
 units_blue = coalition["blue"]
 --blue_planes = mission["coalition"]["blue"]["country"][1]["plane"]["group"]
 
-iterate_coalition(coalition, false)
-
---print(dump(mission["coalition"]["blue"]["country"][1]["plane"]["group"]))
+radios = iterate_coalition_get_radios(coalition, false)
+print(dump(radios))
+print("End script")
